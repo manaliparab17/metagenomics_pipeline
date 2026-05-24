152dc85dea3c716df340a8246bd0ed4a56f69c49
manaliparab17 <manali.parab1706@gmail.com>
2026-05-24 20:10:01 +0530

    Create import_data.py

--- import_data.py
+++ import_data.py
@@ -8,16 +8,22 @@ def import_data():
 
     print("Importing sequences...\n")
 
+    # Import paired-end FASTQ files
+
     subprocess.run("""
     qiime tools import \
     --type 'SampleData[PairedEndSequencesWithQuality]' \
-    --input-path bc_manifest.csv \
+    --input-path bc_manifest.tsv \
     --output-path results/demux.qza \
     --input-format PairedEndFastqManifestPhred33V2
-    """, shell=True)
+    """, shell=True, check=True)
+
+    # Generate demultiplexed sequence summary
 
     subprocess.run("""
     qiime demux summarize \
     --i-data results/demux.qza \
     --o-visualization results/demux.qzv
-    """, shell=True)
+    """, shell=True, check=True)
+
+    print("Import completed.\n")
