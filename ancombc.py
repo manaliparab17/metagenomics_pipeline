efbc2458bae278e2f0d5f9e2cac5fdda3bf41d50
manaliparab17 <manali.parab1706@gmail.com>
2026-05-24 21:29:17 +0530

    Create ancombc.py

--- ancombc.py
+++ ancombc.py
@@ -4,14 +4,38 @@ import os
 
 def run_ancombc():
 
-    print("Preparing files for ANCOM-BC...\n")
+    print("Running ANCOM-BC...\n")
 
     os.makedirs("results/ancombc", exist_ok=True)
 
-    subprocess.run("""
-    qiime tools export \
-    --input-path results/table.qza \
-    --output-path results/ancombc
-    """, shell=True)
+    # Filter samples if needed
 
-    print("Run ANCOM-BC separately in R.\n")
+    subprocess.run("""
+    qiime feature-table filter-samples \
+    --i-table results/table.qza \
+    --m-metadata-file metadata.tsv \
+    --p-where "[group]='Diseased' OR [group]='Healthy'" \
+    --o-filtered-table results/ancombc/filtered-table.qza
+    """, shell=True, check=True)
+
+    # Run ANCOM-BC
+
+    subprocess.run("""
+    qiime composition ancombc \
+    --i-table results/ancombc/filtered-table.qza \
+    --m-metadata-file metadata.tsv \
+    --p-formula group \
+    --o-differentials results/ancombc/ancombc.qza
+    """, shell=True, check=True)
+
+    # Generate ANCOM-BC barplot visualization
+
+    subprocess.run("""
+    qiime composition da-barplot \
+    --i-data results/ancombc/ancombc.qza \
+    --p-significance-threshold 0.001 \
+    --o-visualization results/ancombc/ancombc-barplot.qzv
+    """, shell=True, check=True)
+
+    print("ANCOM-BC analysis completed.\n")
+    print("Differential abundance analysis finished.\n")
